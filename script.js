54140b089c440e79fbc4a836be5adb355d28af26
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-07 23:12:48 +0300

    OOP files

---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict"
+
+// class car {
+// 	color
+// 	fuel
+
+// 	go() {
+
+// 	}
+
+// 	turn() {
+
+// 	}
+
+// 	stop() {
+
+// 	}
+// }
+
+// let myCar = new car; //команда заводу сделать авто.
+// myCar.color = 'red';
+// myCar.fuel = 50;
+
+// myCar.go();
+// myCar.turn();
+// myCar.stop();
+
+class Employee {
+	name
+	age
+	solary
+}
+
+let employee = new Employee;//объект класса
+employee.name = 'Vit';
+employee.age = 41;
+employee.solary = 300;
+console.log(employee);
+
+
+
+
+
+
+
+
+
