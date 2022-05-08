52d8ae3590583187c5783cef91e6d6885d2d3d5c
Vitalick Bokhan <miraigeweb@gmail.com>
2022-05-08 23:47:50 +0300

    initials oop

--- script.js
+++ script.js
@@ -25,34 +25,50 @@
 // myCar.turn();
 // myCar.stop();
 
-class Employee {
-	show(name, solary) {
-		return name + ' ' + solary;
-	}
-	objThis() {
-		console.log(this.name + ' ' + this.solary);
-	}
+// class Employee {
+// 	show(name, solary) {
+// 		return name + ' ' + solary;
+// 	}
+// 	objThis() {
+// 		console.log(this.name + ' ' + this.solary);
+// 	}
+// 	name
+// 	age
+// 	solary
+// }
+
+// let employee1 = new Employee;//объект класса
+// employee1.name = 'Vit';
+// employee1.age = 41;
+// employee1.solary = 300;
+// console.log(employee1);
+
+// let employee2 = new Employee;//объект класса
+// employee2.name = 'Lex';
+// employee2.age = 30;
+// employee2.solary = 255;
+// console.log(employee2);
+// console.log(employee2.show('Jason', 3500));
+// employee2.objThis();
+
+
+
+class Student {
 	name
-	age
-	solary
+	surn
+	show() {
+		return this.initials(this.name + ' ' + this.surn);
+	}
+	initials(str) {
+		console.log(str)
+		return str[0].toUpperCace() + str.slice(1);
+	}
 }
 
-let employee1 = new Employee;//объект класса
-employee1.name = 'Vit';
-employee1.age = 41;
-employee1.solary = 300;
-console.log(employee1);
-
-let employee2 = new Employee;//объект класса
-employee2.name = 'Lex';
-employee2.age = 30;
-employee2.solary = 255;
-console.log(employee2);
-console.log(employee2.show('Jason', 3500));
-employee2.objThis();
-
-
-
+let student = new Student;
+student.name = 'Vitalick';
+student.surn = 'Bokhan';
+student.show();
 
 
 
